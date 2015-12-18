522dd6258d6b03aea11f8db0fcc4e79e29c8bbf1
Nedas Zilinskas <nedas.zilinskas@gmail.com>
2015-12-18 13:44:44 +0200

    .

--- website_sale_variant_pictures/__openerp__.py
+++ website_sale_variant_pictures/__openerp__.py
@@ -1,19 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2015 Nedas Žilinskas <nedas.zilinskas@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
-    'name': 'Variant Pictures',
-    'category': 'Website',
-    'summary': 'Shows picture of the product variant instead of plain color',
-    'version': '1.0',
-    'description': """
+    "name": "Variant Pictures",
+    "category": "Website",
+    "summary": "Shows picture of the product variant instead of plain color",
+    "version": "8.0.1.0",
+    "description": """
 Variant Pictures
 ======================================
 Shows picture of the product variant instead of plain color
         """,
-    'author': 'Nedas Žilinskas <nedas.zilinskas@gmail.com>',
-    'website': 'http://nedaszilinskas.com',
-    'depends': ['website_sale'],
-    'data': [
-        'views/assets.xml',
-        'views/templates.xml'
+    "author": "Nedas Žilinskas <nedas.zilinskas@gmail.com>",
+    "website": "http://nedaszilinskas.com",
+    "depends": ["website_sale"],
+    "data": [
+        "views/assets.xml",
+        "views/templates.xml"
     ],
-    'installable': True
+    "images": [
+        "static/description/main_screenshot.png",
+    ],
+    "installable": True
 }
